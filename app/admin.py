a3ae08553e88dfddc852190e0323d3acdd39c8f0
georgenavarro1802 <gnavarrog1802@gmail.com>
2017-10-12 18:13:37 -0400

    links and back button to the homepage

--- app/admin.py
+++ app/admin.py
@@ -5,28 +5,28 @@ from app.models import Aboutus, Services, Whyus, Projects, Team, Newsletter, Cli
 
 
 class AboutusAdmin(admin.ModelAdmin):
-    list_display = ('words', 'icon1_title', 'icon2_title', 'icon3_title', 'icon4_title')
+    list_display = ('id', 'words', 'icon1_title', 'icon2_title', 'icon3_title', 'icon4_title')
     search_fields = ('words', 'icon1_title', 'icon2_title', 'icon3_title', 'icon4_title')
 
 admin.site.register(Aboutus, AboutusAdmin)
 
 
 class ServicesAdmin(admin.ModelAdmin):
-    list_display = ('header1', 'header2', 'icon1_title', 'icon2_title', 'icon3_title')
+    list_display = ('id', 'header1', 'header2', 'icon1_title', 'icon2_title', 'icon3_title')
     search_fields = ('icon1_title', 'icon2_title', 'icon3_title')
 
 admin.site.register(Services, ServicesAdmin)
 
 
 class WhyusAdmin(admin.ModelAdmin):
-    list_display = ('header1', 'header2', 'icon1_title', 'icon2_title', 'icon3_title', 'icon4_title')
+    list_display = ('id', 'header1', 'header2', 'icon1_title', 'icon2_title', 'icon3_title', 'icon4_title')
     search_fields = ('icon1_title', 'icon2_title', 'icon3_title', 'icon4_title')
 
 admin.site.register(Whyus, WhyusAdmin)
 
 
 class ProjectsAdmin(admin.ModelAdmin):
-    list_display = ('header1', 'header2', 'icon1_title', 'icon2_title', 'icon3_title', 'icon4_title',
+    list_display = ('id', 'header1', 'header2', 'icon1_title', 'icon2_title', 'icon3_title', 'icon4_title',
                     'icon5_title', 'icon6_title', 'icon7_title', 'icon8_title')
     search_fields = ('icon1_title', 'icon2_title', 'icon3_title', 'icon4_title')
 
@@ -34,14 +34,14 @@ admin.site.register(Projects, ProjectsAdmin)
 
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('header1', 'header2', 'icon1_name', 'icon2_name', 'icon3_name', 'icon4_name')
+    list_display = ('id', 'header1', 'header2', 'icon1_name', 'icon2_name', 'icon3_name', 'icon4_name')
     search_fields = ('icon1_name', 'icon2_name', 'icon3_name', 'icon4_name')
 
 admin.site.register(Team, TeamAdmin)
 
 
 class NewsletterAdmin(admin.ModelAdmin):
-    list_display = ('email', 'created')
+    list_display = ('id', 'email', 'created')
     search_fields = ('email', )
     ordering = ('created', )
 
@@ -49,7 +49,7 @@ admin.site.register(Newsletter, NewsletterAdmin)
 
 
 class ClientsAdmin(admin.ModelAdmin):
-    list_display = ('header1', 'header2')
+    list_display = ('id', 'header1', 'header2')
     search_fields = ('header1', 'header2')
 
 admin.site.register(Clients, ClientsAdmin)
@@ -65,28 +65,28 @@ admin.site.register(Contacts, ContactsAdmin)
 
 
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'address', 'phone')
+    list_display = ('id', 'name', 'email', 'address', 'phone')
     search_fields = ('name', 'address')
 
 admin.site.register(Company, CompanyAdmin)
 
 
 class ConstructionAdmin(admin.ModelAdmin):
-    list_display = ('header1', 'header2', 'subservices')
+    list_display = ('id', 'header1', 'header2', 'subservices')
     search_fields = ('header1', 'header2')
 
 admin.site.register(Construction, ConstructionAdmin)
 
 
 class RemodelingAdmin(admin.ModelAdmin):
-    list_display = ('header1', 'header2', 'subservices')
+    list_display = ('id', 'header1', 'header2', 'subservices')
     search_fields = ('header1', 'header2')
 
 admin.site.register(Remodeling, RemodelingAdmin)
 
 
 class VoltageAdmin(admin.ModelAdmin):
-    list_display = ('header1', 'header2', 'subservices')
+    list_display = ('id', 'header1', 'header2', 'subservices')
     search_fields = ('header1', 'header2')
 
 admin.site.register(Voltage, VoltageAdmin)
